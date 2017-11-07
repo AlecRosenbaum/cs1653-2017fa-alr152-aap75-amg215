1aa0edaebf906b57aeb8cbbcce661ea5af2a954c
Alex George <amg215@pitt.edu>
2017-11-07 09:04:19 -0500

    fixed conflict

--- src/RunClient.java
+++ src/RunClient.java
@@ -1,10 +1,15 @@
 /* Driver program for Client */
 import java.util.ArrayList;
 import java.io.*;
+import java.security.PublicKey;
 import java.util.*;
+import java.lang.*;
+import java.util.concurrent.TimeUnit;
 
 
 public class RunClient {
+
+    private static String fileServersPath = "APPROVED_FILE_SERVERS.bin";
 
     public static void main(String[] args) {
         // args:
@@ -23,7 +28,25 @@ public class RunClient {
         String group_server_url = args[0];;
         int group_server_port;
         String file_server_url = args[2];
-        int file_server_port;
+        int file_server_port;                
+        GroupClient group_client = new GroupClient();        
+        FileClient file_client = new FileClient();
+        FileClient new_file_client = new FileClient();
+        PublicKey fileServerPublicKey = null;
+        ApprovedFileServerList approvedFileServers = new ApprovedFileServerList();
+        try {
+            File approvedFileServersFile = new File(fileServersPath);
+            if(approvedFileServersFile.exists() && !approvedFileServersFile.isDirectory()) { 
+                FileInputStream fis = new FileInputStream(fileServersPath);
+                ObjectInputStream fileStream = new ObjectInputStream(fis);
+                approvedFileServers = (ApprovedFileServerList)fileStream.readObject();
+            }
+        } catch(Exception e) {
+            System.out.println("Error loading approved files.");            
+            System.out.println(e.getCause());
+            return;
+        }
+        
         try  {
             group_server_port = Integer.parseInt(args[1]);
             file_server_port = Integer.parseInt(args[3]);
@@ -34,25 +57,116 @@ public class RunClient {
             return;
         }
 
+        
+        boolean success = false;
+        boolean firstTime = true;        
         // instantiate file client and group client
-        System.out.println("Connecting to group server...");
-        GroupClient group_client = new GroupClient();
-        if (group_client.connect(group_server_url, group_server_port)) {
-            System.out.println("Connected to group server " + group_server_url + ":" + group_server_port);
-        } else {
-            System.out.println("Unable to connect to group server " + group_server_url + ":" + group_server_port);
-        }
+        do {
+            if(!firstTime) {
+                System.out.print("Enter a file server url: ");
+                group_server_url = console.nextLine();
+                System.out.print("Enter a file server port: ");
+                group_server_port = Integer.valueOf(console.nextLine());
+            }
+            firstTime = false;
+            group_client = new GroupClient();        
+            System.out.println("Connecting to group server...");
+            if (group_client.connect(group_server_url, group_server_port, null)) {
+                System.out.println("Connected to group server " + group_server_url + ":" + group_server_port);
+                success = true;
+            } else {
+                System.out.println("Unable to connect to group server " + group_server_url + ":" + group_server_port);
+            }
+        } while (!success);  
 
         System.out.println("Connecting to file server...");
-        FileClient file_client = new FileClient();
-        if (file_client.connect(file_server_url, file_server_port)) {
-            System.out.println("Connected to file server " + file_server_url + ":" + file_server_port);
-        } else {
-            System.out.println("Unable to connect to file server " + file_server_url + ":" + file_server_port);
-        }
-
+        success = false;
+        firstTime = true;        
+        do {
+            if(!firstTime) {
+                System.out.print("Enter a file server url: ");
+                file_server_url = console.nextLine();
+                System.out.print("Enter a file server port: ");
+                file_server_port = Integer.valueOf(console.nextLine());
+            }
+            firstTime = false;            
+            file_client = new FileClient();
+            new_file_client = new FileClient();
+            fileServerPublicKey = approvedFileServers.checkServer(file_server_url, file_server_port);
+            boolean connectToFileServer = true;
+            if(fileServerPublicKey == null) {
+                fileServerPublicKey = new_file_client.initialConnect(file_server_url, file_server_port);
+                System.out.print("Server Provided Public Key For Authentication " + fileServerPublicKey.toString() + "\n\n" +
+                "Enter 'Y' to accept or 'N' to reject: ");
+                if(console.nextLine().toUpperCase().equals("Y")) {
+                    approvedFileServers.addServer(fileServerPublicKey, file_server_url, file_server_port);
+                    new_file_client = new FileClient();
+                }else {
+                    System.out.println("File Server Public Key NOT approved.");
+                    connectToFileServer = false;
+                }
+            }
+            if(connectToFileServer){
+                if (new_file_client.connect(file_server_url, file_server_port, fileServerPublicKey)) {
+                    System.out.println("Connected to file server " + file_server_url + ":" + file_server_port);
+                    file_client = new_file_client;
+                    success = true;
+                } else {
+                    System.out.println("Unable to connect to file server " + file_server_url + ":" + file_server_port);
+                }
+            }
+        } while (!success);
+        
         System.out.print("Enter user name: ");
         String u = console.nextLine();
+        if(group_client.isLocked(u) == true) {
+        	
+        	System.out.println("Your account has been locked due to too many failed login attempts. Please contact an administrator to unlock");
+			try {
+				TimeUnit.SECONDS.sleep(3);
+                group_client.disconnect();
+                file_client.disconnect();
+                return;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+        }
+        System.out.println("Please enter your password: ");
+		String password = console.next();
+		if(group_client.checkPassword(u, password) == false) {
+			int tries = 4;
+			do {
+				System.out.println("Password Incorrect, you have " + tries + "more attempts before being locked out" );
+				password = console.next();
+				tries --;
+				
+			}while(group_client.checkPassword(u, password) == false && tries > 0);
+			if(group_client.checkPassword(u, password) == false) {
+				group_client.lockUser(u);
+				System.out.println("Your account has been locked due to too many failed login attempts. Please contact an administrator to unlock");
+				try {
+					TimeUnit.SECONDS.sleep(3);
+                    group_client.disconnect();
+                    file_client.disconnect();
+                    return;
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		if(group_client.needsPassword(u) == true) {
+			
+			System.out.println("Please enter a new password, must be between 8-16 characters: ");
+			String newPassword = console.next();
+			if(newPassword.length() <8 || newPassword.length() > 16) {
+				
+				do {
+					System.out.println("Please enter a valid password, must be between 8-16 characters: ");
+					newPassword = console.next();
+					
+				}while(newPassword.length() <8 || newPassword.length() > 16);
+			}
+		}
         UserToken mytoken = group_client.getToken(u);
         if (mytoken == null) {
             System.out.println("Login Unsuccessful");
@@ -119,11 +233,24 @@ public class RunClient {
                                          "\t\tLists all users in given group.\n\n"
                                         );
                         break;
-                    case "fconnect":
+                    case "fconnect":                        
                         url = inputArray[1];
                         port = Integer.parseInt(inputArray[2]);
-                        FileClient new_file_client = new FileClient();
-                        if (new_file_client.connect(url, port)) {
+                        new_file_client = new FileClient();
+                        fileServerPublicKey = approvedFileServers.checkServer(url, port);
+                        if(fileServerPublicKey == null) {
+                            fileServerPublicKey = new_file_client.initialConnect(url, port);
+                            System.out.print("Server Provided Public Key For Authentication " + fileServerPublicKey.toString() + "\n\n" +
+                            "Entery 'Y' to accept or 'N' to reject: ");
+                            if(console.nextLine().toUpperCase().equals("Y")) {
+                                approvedFileServers.addServer(fileServerPublicKey, url, port);
+                                new_file_client = new FileClient();
+                            }else {
+                                System.out.println("File Server Public Key NOT approved.");
+                                break;
+                            }
+                        }
+                        if (new_file_client.connect(url, port, fileServerPublicKey)) {
                             System.out.println("Connected to file server " + url + ":" + port);
                             file_client = new_file_client;
                             file_server_url = url;
@@ -165,7 +292,7 @@ public class RunClient {
                         url = inputArray[1];
                         port = Integer.parseInt(inputArray[2]);
                         GroupClient new_group_client = new GroupClient();
-                        if (new_group_client.connect(url, port)) {
+                        if (new_group_client.connect(url, port, null)) {
                             System.out.println("Connected to group server " + url + ":" + port);
                             group_client = new_group_client;
                             group_server_url = url;
@@ -190,8 +317,10 @@ public class RunClient {
                         break;
                     case "createuser":
                         userName = inputArray[1];
-                        if (group_client.createUser(userName, mytoken)) {
+                        String randomPassword = getRandomString();
+                        if (group_client.createUser(userName, randomPassword , mytoken)) {
                             System.out.println("Created user " + userName);
+                            System.out.println("New user's temporary password: " + randomPassword);
                         } else {
                             System.out.println("User creation failed.");
                         }
@@ -227,6 +356,14 @@ public class RunClient {
                             System.out.println("Added user " + userName + " to " + groupName);
                         } else {
                             System.out.println("Unable to add user to group");
+                        }
+                        break;
+                    case "unlock":
+                        userName = inputArray[1];
+                        if (group_client.unlockUser(userName,mytoken)) {
+                            System.out.println("User Unlocked!");
+                        } else {
+                            System.out.println("Unable to unlock user");
                         }
                         break;
                     case "deletegroupuser":
@@ -300,15 +437,6 @@ public class RunClient {
             }
             System.out.println("---------------");
 
-            // add user to group
-            group_client.createUser("new_user", mytoken);
-            if (group_client.addUserToGroup("new_user", "test_group", mytoken)) {
-                System.out.println("User added to group.");
-            } else {
-                System.out.println("User not added to group.");
-                return;
-            }
-
             // list members
             members = (ArrayList<String>)group_client.listMembers("test_group", mytoken);
             for (String member : members) {
@@ -343,9 +471,60 @@ public class RunClient {
             group_client.disconnect();
             file_client.disconnect();
         }
+    }
+    public static String getRandomString() {
+		
+  		Random rand = new Random();
+  		StringBuilder sb = new StringBuilder();
+  		String charList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  		for(int i = 0; i < 8; i++) {
+  			
+  			int charN = rand.nextInt(charList.length());
+  			sb.append(charList.charAt(charN));
+  		}
+  		
+  		return sb.toString();
+  	}
 
+}
 
+class ApprovedFileServerList implements java.io.Serializable {
+    private static final long serialVersionUID = -8911161283900345136L;
+    ArrayList<PublicKey> pks;
+    ArrayList<String> urls;
+    ArrayList<Integer> ports;
+    private static String fileServersPath = "APPROVED_FILE_SERVERS.bin";
+
+    public ApprovedFileServerList () {
+        pks = new ArrayList<PublicKey>();
+        urls = new ArrayList<String>();
+        ports = new ArrayList<Integer>();
     }
 
+    public void addServer(PublicKey new_pk, String new_url, int new_port) {
+        pks.add(new_pk);
+        urls.add(new_url);
+        ports.add(new Integer(new_port));
+        backup();
+    }
 
+    public PublicKey checkServer(String url, int port) {
+        if(urls.contains(url) && ports.contains(new Integer(port)))
+        {
+            if(urls.indexOf(url) == ports.indexOf(new Integer(port))) {
+                return pks.get(urls.indexOf(url));
+            }
+        }
+        return null;
+    }
+
+    public void backup() {
+        try {
+            ObjectOutputStream outStream = new ObjectOutputStream(new FileOutputStream(fileServersPath));
+            outStream.writeObject(this);
+        } catch(Exception e) {
+            System.out.println("Error backing up approved files");
+        }
+        
+    }
 }
